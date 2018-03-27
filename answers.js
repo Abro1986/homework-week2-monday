e4486568291869b5dac0648a03dee2f107553bee
Abro1986 <AndrewBroestl@gmail.com>
2018-03-26 18:08:27 -0600

    2nd commit - created some data structures and accessed some data.

---
+++ answers.js
@@ -0,0 +1,25 @@
+//DataTypes
+
+//1. boolean
+//2. string
+//3. object
+//4. array
+//5. object
+//6. object
+
+//Take it Easy
+
+let array = ['r', 'o', 'y', 'g', 'b', 'i', 'v' ]
+
+console.log(array[4]);
+
+const meObj = {
+	name: "Drew",
+	favoriteFood: "Steak",
+	hobby: "Fly fishing",
+	town: "Fort Collins",
+	dataType: "Objects duh"
+
+}
+
+console.log(meObj.hobby);
